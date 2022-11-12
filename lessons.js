3c7da6dee3795738349e117885c7b27b9f6e7362
Abdul Haseeb Khan <haseeb29jan@gmail.com>
2022-11-12 10:55:39 +0400

    First commit

---
+++ lessons.js
@@ -0,0 +1,82 @@
+let lessonsArr = [
+    {
+        id: 1001,
+        subject: "Arduino",
+        location: "Dubai",
+        price: 2000,
+        spaces: 5,
+        image: "images/arduino.png"
+    },
+    {
+        id: 1002,
+        subject: "Python",
+        location: "Abu Dhabi",
+        price: 3000,
+        spaces: 5,
+        image: "images/python.png"
+    },
+    {
+        id: 1003,
+        subject: "Physical Education",
+        location: "Sharjah",
+        price: 1500,
+        spaces: 5,
+        image: "images/pe.png"
+    },
+    {
+        id: 1004,
+        subject: "UI/UX",
+        location: "Riyadh",
+        price: 3000,
+        spaces: 5,
+        image: "images/uiux.png"
+    },
+    {
+        id: 1005,
+        subject: "Data Structures & Algorithms",
+        location: "Ohio",
+        price: 3500,
+        spaces: 5,
+        image: "images/dsa.png"
+    },
+    {
+        id: 1006,
+        subject: "Psychology",
+        location: "Delhi",
+        price: 1000,
+        spaces: 5,
+        image: "images/psychology.png"
+    },
+    {
+        id: 1007,
+        subject: "Art 101",
+        location: "Paris",
+        price: 2500,
+        spaces: 5,
+        image: "images/art.png"
+    },
+    {
+        id: 1008,
+        subject: "Game Development",
+        location: "Tbilisi",
+        price: 3000,
+        spaces: 5,
+        image: "images/game.png"
+    },
+    {
+        id: 1009,
+        subject: "Virtual Reality",
+        location: "London",
+        price: 2000,
+        spaces: 5,
+        image: "images/vr.png"
+    },
+    {
+        id: 1010,
+        subject: "Augmented Reality",
+        location: "Karachi",
+        price: 2500,
+        spaces: 5,
+        image: "images/ar.png"
+    },
+];
